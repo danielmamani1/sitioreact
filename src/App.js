b1ecadc8e7b4b4a4f25765f1443e090a07c553e2
Daniel Mamani Chino <mjdaniel.2323@gmail.com>
2023-10-24 13:42:18 -0500

    version 1 de git

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import { collection, getDocs, query, doc, deleteDoc, where, } from "firebase/fir
 import React, {useEffect, useState} from 'react';
 import Firebase, { db } from './componentes/firebase';
 import AppForm from './componentes/AppForm';
-import { useEffect, useState } from 'react';
 
 
-function App() {
+
+
 
 return (
   <div style={{width:"350px", background:"greenyellow", padding:"10px"}}>
     <AppForm/>
   </div>
 );
-}
+
 export default App;
